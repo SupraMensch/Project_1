44921739ec8c326dbe5d4fc4fb6f74d749ab9dab
SupraMensch <cwadeburt@gmail.com>
2018-10-10 21:10:11 -0500

    Instructions and name

---
+++ src/project_1/Project_1.java
@@ -0,0 +1,26 @@
+// Christopher Burt
+
+//Projecr_1
+
+//
+
+//  Search an Array (20 points)
+//
+//  Create an array of Strings that contains the following:
+//
+//  String[] names = { "Bill", "Mike", "Joe", "Dave", "Sam", "Mary", "George", "Lucy" }
+//
+//  In your main program, search the array and display all of the elements that begin with 'M'.
+
+
+package project_1;
+
+
+public class Project_1 {
+
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
