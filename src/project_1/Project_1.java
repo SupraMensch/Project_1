7d3578cf3f945f79f9ab1a3c09815731fb4017b1
SupraMensch <cwadeburt@gmail.com>
2018-10-13 13:22:33 -0500

    draft1

--- src/project_1/Project_1.java
+++ src/project_1/Project_1.java
@@ -16,11 +16,21 @@
 package project_1;
 
 
-public class Project_1 {
+public class Project_1 
+{
 
     public static void main(String[] args)
     {
-        // TODO code application logic here
-    }
+        String[] names = { "Bill", "Mike", "Joe", "Dave", "Sam", "Mary", "George", "Lucy" };
+        
+       for(int i = 0; i < names.length; i++)
+       {
+            if(names[i].indexOf('M') != -1)
+            {
+                System.out.println(names[i]);
+            }
+        }
+        
+     }
     
 }
